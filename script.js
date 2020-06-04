2e35a5bc5318497b49b465a684162c87d0bd04f0
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-04 15:38:54 -0500

    practice

---
+++ script.js
@@ -0,0 +1,28 @@
+var d = new Date()
+const time = d.getMinutes();
+let hour = d.getHours();
+var dayList = ["Sunday" , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+if(hour >= 12){
+  hour = hour - 12
+
+
+console.log("Today is: " + dayList[d.getDay()] + " " + hour + ":" + time + " pm");
+}
+var body = document.querySelector('body');
+
+var div = document.createElement('div');
+
+div.innerHTML = "Helloo";
+
+
+div.style.backgroundColor = 'Green';
+
+document.body.appendChild(div);
+
+var button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+  window.print()
+})
