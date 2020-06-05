0527106a391a61b053b1bb77935d0bab35552a3a
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-04 22:06:41 -0500

    made my leap year function

--- q_6/script.js
+++ q_6/script.js
@@ -5,7 +5,8 @@ var year = d.getFullYear()
 // console.log(2020 % 4)
 function leapYear (theYear){
     if(theYear % 4 == 0 && theYear % 100 == 0 && theYear % 400 == 0){
-        return theYear
+        
     }
+    return console.log(theYear)
 }
 alert(leapYear(year));
