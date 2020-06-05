069c60bcd803411f76e008ff9a696831572e67bf
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-05 16:38:13 -0500

    it worked

--- q_5/script.js
+++ q_5/script.js
@@ -1,36 +1,16 @@
-// const sstring = 'w3resource';
-
-// console.log(sstring.substring(1, 3))
-
 var word = document.getElementById('target');
-
-// console.log(word.innerText)
-
-// console.log(word.innerText.substring(10, 0))
-
-// var theSplit = word.split(",");
-
-// console.log(theSplit)
-
 var arr = Array.from(word.innerText);
-// conssdfsole.log(arr)
-var newArr = []
-
-function rearrangement(arr1, arr2){
-    for(var i = 9; i < arr1.length; i--){
-        arr2.unshift(arr1.pop(i))
-        
-    }
-    return arr2
-}
-
-console.log(rearrangement(arr, newArr))
-
-// var test = [1, 2, 3, 4]
+console.log(arr)
 // var newArr = []
-// console.log(test.pop())
 
-// for(let i = 0; i < test.length; i++){
+// function rearrangement(arr1, arr2)
+// {
+//     for(var i = 9; i < arr1.length; i--)
+//         arr2.unshift(arr1.pop(i))
 
+//     return arr2
 // }
 
+// console.log(rearrangement(arr, newArr))
+
+console.log(arr.reverse())
