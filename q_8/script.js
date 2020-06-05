a09307689147f916bb6c60712f934d961c2809dc
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-05 13:58:18 -0500

    made a guessing game

--- q_8/script.js
+++ q_8/script.js
@@ -1,2 +1,17 @@
 //Gonna have to use math.random
 //need to use input
+
+var input = prompt("Guess a number between 1-10")
+
+function randomNumber(theInput){
+    var number = Math.floor((Math.random() * 10) + 1);
+
+    if(theInput == number){
+        return console.log("You guessed right!")
+    }
+    else{
+        return console.log("Guess again :/")
+    }
+}
+
+randomNumber(input);
