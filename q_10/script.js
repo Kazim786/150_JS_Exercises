ee29b93e30b280bed15c8b08acea7fec9dd9fe61
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-08 11:55:04 -0500

    made separate buttons for multiply and divide

---
+++ q_10/script.js
@@ -0,0 +1,11 @@
+const multButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
+const firstNum = document.getElementById('firstNum')
+const secondNum = document.getElementById('secondNum') 
+
+function theMath(first, second){
+    if(button)
+
+}
+
+button.addEventListener('click')
