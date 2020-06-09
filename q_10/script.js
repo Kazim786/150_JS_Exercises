0b30712bc2455f5fe2c156b4b7a2605e9d17dcef
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-09 17:47:13 -0500

    used the stackoverflow debug solution

--- q_10/script.js
+++ q_10/script.js
@@ -1,3 +1,25 @@
+// const multButton = document.getElementById('multiply');
+// const divideButton = document.getElementById('divide');
+// const firstNum = document.getElementById('firstNum')
+// const secondNum = document.getElementById('secondNum') 
+
+
+
+// function multiplyNum(first, second){
+//     const sum = first * second;
+//     return alert(sum);
+// }
+
+// function divideNum(first, second){
+//     const sum = first/second;
+//     return alert(sum);
+// }
+
+// multButton.addEventListener('click', multiplyNum)
+
+// divideButton.addEventListener('click', divideNum)
+
+
 const multButton = document.getElementById('multiply');
 const divideButton = document.getElementById('divide');
 const firstNum = document.getElementById('firstNum')
@@ -5,16 +27,15 @@ const secondNum = document.getElementById('secondNum')
 
 
 
-function multiplyNum(first, second){
-    const sum = first * second;
+function multiplyNum(){
+    const sum = firstNum.value * secondNum.value;
     return alert(sum);
 }
 
-function divideNum(first, second){
-    const sum = first/second;
+function divideNum(){
+    const sum = firstNum.value/secondNum.value;
     return alert(sum);
 }
-
 multButton.addEventListener('click', multiplyNum)
 
 divideButton.addEventListener('click', divideNum)
