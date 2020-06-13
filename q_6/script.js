6c731f1429498989af922b0c05187c170b9fe45d
Kazim Shabbir <59815134+Kazim786@users.noreply.github.com>
2020-06-13 13:57:30 -0500

    Update script.js

--- q_6/script.js
+++ q_6/script.js
@@ -3,10 +3,15 @@ var d = new Date();
 var year = d.getFullYear()
 
 // console.log(2020 % 4)
+
+
 function leapYear (theYear){
     if(theYear % 4 == 0 && theYear % 100 == 0 && theYear % 400 == 0){
         
     }
+    else {
+      alert("Its not leap year");
+    }
     return console.log(theYear)
-}
+} 
 alert(leapYear(year));
