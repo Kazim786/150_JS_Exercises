7ad3e3b54cf059adb825bc8bc5fb318c5a360fe6
Kazim Shabbir <59815134+Kazim786@users.noreply.github.com>
2020-06-13 14:04:49 -0500

    Update script.js

--- q_8/script.js
+++ q_8/script.js
@@ -3,15 +3,31 @@
 
 var input = prompt("Guess a number between 1-10")
 
+
 function randomNumber(theInput){
     var number = Math.floor((Math.random() * 10) + 1);
 
-    if(theInput == number){
+    if(theInput === number){
         return console.log("You guessed right!")
     }
     else{
-        return console.log("Guess again :/")
+        return console.log(`Guess again :/ the number was ${number}`)
     }
 }
 
 randomNumber(input);
+
+
+//Old code
+// function randomNumber(theInput){
+//     var number = Math.floor((Math.random() * 10) + 1);
+
+//     if(theInput == number){
+//         return console.log("You guessed right!")
+//     }
+//     else{
+//         return console.log("Guess again :/")
+//     }
+// }
+
+// randomNumber(input);
